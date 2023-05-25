047088c6b75ced2496781dd49051afd1f3d58a3a
JasonStephen <b1685941616@gmail.com>
2023-05-25 19:15:19 +0800

    Updating

--- src/main/java/com/wzbc/config/MyLocalResovel.java
+++ src/main/java/com/wzbc/config/MyLocalResovel.java
@@ -1,2 +1,40 @@
-package com.wzbc.config;public class MyLocalResove {
+package com.wzbc.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class MyLocalResovel implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        String l = httpServletRequest.getParameter("l");
+        String header = httpServletRequest.getHeader("Accept-Language");
+        Locale locale = null;
+
+        if(!StringUtils.isEmpty(l)){
+            String[] split = l.split("_");
+            locale = new Locale(split[0],split[1]);
+        }else{
+            String[] split = header.split(",");
+            String[] split1 = split[0].split("-");
+            locale = new Locale(split1[0],split1[1]);
+        }return locale;
+    }
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest,
+        @Nullable HttpServletResponse httpServletResponse,
+        @Nullable Locale locale) {
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocalResovel();
+    }
 }
--- src/main/java/com/wzbc/controller/LoginController.java
+++ src/main/java/com/wzbc/controller/LoginController.java
@@ -1,2 +1,16 @@
-package com.wzbc.controller;public class LoginController {
+package com.wzbc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+
+@Controller
+public class LoginController {
+    @GetMapping("/toLogin")
+    public String toLoginPage(Model model){
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+    }
 }
